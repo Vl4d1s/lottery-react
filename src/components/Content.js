94474eae71c2c1ff45ae77d5a739dda25d42acdc
Vladis <vladismarkin@gmail.com>
2021-02-07 20:18:56 +0200

    more changes

--- src/components/Content.js
+++ src/components/Content.js
@@ -17,7 +17,7 @@ const Content = (props) => {
       <h1 className="ui header">💸 Lottery Contract 💸</h1>
       <h2 className="line">{`
                           In this account you can buy lottery tickets at a cost of 0.1 site per card. 
-                          Then, the lottery manager will randomly grill a winner and the money will go into his account 💰.`}</h2>
+                          Then, the lottery manager will randomly choose a winner and the money will go into his account 💰.`}</h2>
       <div className="paragraph">
         <div className="line">
           {`The contract address is: `}
